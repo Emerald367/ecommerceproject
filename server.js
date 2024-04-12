3a19e9bfda9c4a4a7a33bdca9fda66717448a5a8
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-04-12 11:18:15 -0700

    installing nodemon and adding PORT number to server

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
