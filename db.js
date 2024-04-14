1a22e8618dee0cd08349f202e72b887caabd2c33
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-04-14 13:01:23 -0700

    Connected database to the project

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+     user: process.env.DB_USER,
+     host: 'localhost',
+     database: process.env.DB_NAME,
+     password: process.env.DB_PASSWORD,
+     port: 5432
+});
+
+function connectToDb() {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT NOW()', (err, res) => {
+            if(err) {
+                console.log('Error connecting to the database', err);
+                reject(err);
+            } else {
+                console.log('Connection Successful, Server Time:', res.rows[0].now)
+            }
+        });
+    });
+}
+
+module.exports = {connectToDb, pool};
--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const db = require('./db')
+const env = require('dotenv').config
+const {pool} = require('./db.js')
 const PORT = 5000;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
+
+module.exports = app;
